4d67f9f00f37408ab4e76be999269a8e8a08d719
Deano <me@deanoc.com>
2019-09-15 15:46:47 +0300

    Render basics stock WIP

--- include/render_basics/api.h
+++ include/render_basics/api.h
@@ -1,46 +1,112 @@
 #pragma once
 
+/// Render basics provides higher level render function than the direct gfx
+/// libraries. Its also begins to abstract the actual gfx backend (currently
+/// always TheForge). Its not designed to provide every function the gfx backend
+/// does for that simply drop to its level. However it provides much of the glue
+/// for most common cases.
+/// Also provide 'stocks' objects for many of the common uses of samplers etc.
+
 #include "al2o3_platform/platform.h"
 #include "tiny_imageformat/tinyimageformat_base.h"
 
 typedef struct Render_Renderer * Render_RendererHandle;
 typedef struct Render_FrameBuffer * Render_FrameBufferHandle;
-typedef struct Render_RenderTarget * Render_RenderTargetHandle;
+
+typedef struct Render_BlendState *Render_BlendStateHandle;
 typedef struct Render_CmdPool * Render_CmdPoolHandle;
 typedef struct Render_Cmd * Render_CmdHandle;
+typedef struct Render_DepthState *Render_DepthStateHandle;
 typedef struct Render_Queue * Render_QueueHandle;
-
-typedef struct Render_Globals {
-	float time;
-} Render_Globals;
+typedef struct Render_RasteriserState *Render_RasteriserStateHandle;
+typedef struct Render_RenderTarget *Render_RenderTargetHandle;
+typedef struct Render_Sampler *Render_SamplerHandle;
+typedef struct Render_VertexLayout *Render_VertexLayoutHandle;
 
 typedef enum Render_GraphicsQueueType {
-	RENDER_GQT_GRAPHICS,
-	RENDER_GQT_COMPUTE,
-	RENDER_GQT_BLITTER
+	Render_GQT_GRAPHICS,
+	Render_GQT_COMPUTE,
+	Render_GQT_BLITTER
 } Render_GraphicsQueueType;
 
-typedef struct Render_FrameBufferDesc {
-	void * platformHandle;						///< platform specific for the window/display (HWND etc.)
-	Render_QueueHandle queue;					///< queue present will be from
-	Render_CmdPoolHandle commandPool; ///< pool to use for new frame cmd
-	uint32_t frameBufferWidth;				///< size of the framebuffer horizontally in pixels
-	uint32_t frameBufferHeight;				///< size of the framebuffer vertically in pixels
-	uint32_t frameBufferCount;				///< how many frames can be buffered
-	TinyImageFormat colourFormat;			///< swap chain format, UNDEFINED == platform recommended
-	TinyImageFormat depthFormat;			///< swap chain depth format, UNDEFINED == none
-} Render_FrameBufferDesc;
+struct Render_FrameBufferDesc; // forward decl. Definition in framebuffer.h
 
 AL2O3_EXTERN_C Render_RendererHandle Render_RendererCreate();
 
-AL2O3_EXTERN_C char const * const Render_RendererGetBackendName(Render_RendererHandle handle);
-AL2O3_EXTERN_C char const * const Render_RendererGetGPUName(Render_RendererHandle handle);
-AL2O3_EXTERN_C Render_QueueHandle Render_RendererGetPrimaryQueue(Render_RendererHandle ctx, Render_GraphicsQueueType queueType);
-AL2O3_EXTERN_C Render_CmdPoolHandle Render_RendererGetPrimaryCommandPool(Render_RendererHandle handle, Render_GraphicsQueueType queueType);
+// renderer functions
+AL2O3_EXTERN_C char const *Render_RendererGetBackendName(Render_RendererHandle renderer);
+AL2O3_EXTERN_C char const *Render_RendererGetGPUName(Render_RendererHandle renderer);
+AL2O3_EXTERN_C Render_QueueHandle Render_RendererGetPrimaryQueue(Render_RendererHandle renderer,
+																																 Render_GraphicsQueueType queueType);
+AL2O3_EXTERN_C Render_CmdPoolHandle Render_RendererGetPrimaryCommandPool(Render_RendererHandle renderer,
+																																				 Render_GraphicsQueueType queueType);
 
-AL2O3_EXTERN_C void Render_RendererDestroy(Render_RendererHandle handle);
-AL2O3_EXTERN_C Render_FrameBufferHandle Render_FrameBufferCreate(Render_RendererHandle handle,Render_FrameBufferDesc const* desc);
-AL2O3_EXTERN_C void Render_FrameBufferDestroy(Render_FrameBufferHandle handle);
+// creation functions
+AL2O3_EXTERN_C Render_FrameBufferHandle Render_FrameBufferCreate(Render_RendererHandle renderer,
+																																 Render_FrameBufferDesc const *desc);
 
+// destruction functions
+AL2O3_EXTERN_C void Render_RendererDestroy(Render_RendererHandle renderer);
+AL2O3_EXTERN_C void Render_FrameBufferDestroy(Render_RendererHandle renderer, Render_FrameBufferHandle framebuffer);
 
-AL2O3_EXTERN_C void Render_CmdBindRenderTargets(Render_CmdHandle cmd, uint32_t count, Render_RenderTargetHandle* targets, bool clear, bool setViewports, bool setScissors);
+// stock interface
+
+typedef enum Render_StockBlendType {
+	Render_SBT_Opaque, // opaque destination is replaced, single render targets
+	Render_SBT_PorterDuff, // classic alpha blend, single render target
+	Render_SBT_Additive, // src + dest all channels, single render target
+	Render_SBT_PorterDuffPM, // combined blend + add. useful for particles
+
+	Render_SSB_COUNT
+} Render_StockBlendType;
+
+typedef enum Render_StockDepthStateType {
+	Render_SDS_IGNORE, // complete ignore (no read or write) any depth buffer
+	Render_SDS_READONLY_LESS, // test the depth buffer, less comparision
+	Render_SDS_READWRITE_LESS, // test as read only less and also write the depth buffer
+	Render_SDS_READONLY_GREATER, // test the depth buffer, greater comparision
+	Render_SDS_READWRITE_GREATER, // test as read only greater and also write the depth buffer
+	Render_SDS_WRITEONLY, // write the depth buffer unconditionally
+	Render_SDS_COUNT
+} Render_StockDepthStateType;
+
+typedef enum Render_StockRasterState {
+
+	Render_SRS_NOCULL, // filled no culling
+	Render_SRS_BACKCULL, // filled back face culling
+	Render_SRS_FRONTCULL, // filled front face culling
+
+	Render_SRS_COUNT
+} Render_StockRasterState;
+
+typedef enum Render_StockSamplerType {
+	Render_SST_POINT,  // point for all filters. clamp to edge all axis
+	Render_SST_LINEAR, // linear for all filter. clamp to edge all axis
+
+	Render_SST_COUNT
+} Render_StockSamplerType;
+
+typedef enum Render_StockVertexLayouts {
+	Render_SVL_2D_COLOUR, // 2D + packed colour
+	Render_SVL_2D_FLOATCOLOUR, // 2D + float colour
+	Render_SVL_2D_COLOUR_UV, // 2D + packed colour + 2D float UV
+	Render_SVL_2D_FLOATCOLOUR_UV, // 2D + float colour + 2D float UV
+
+	Render_SVL_3D_COLOUR, // 3D + packed colour
+	Render_SVL_3D_FLOATCOLOUR, // 3D + float colour
+	Render_SVL_3D_COLOUR_UV, // 3D + packed colour + 2D float UV
+	Render_SVL_3D_FLOATCOLOUR_UV, // 3D + float colour + 2D float UV
+
+	Render_SVL_COUNT
+} Render_StockVertexLayouts;
+
+AL2O3_EXTERN_C Render_BlendStateHandle Render_GetStockBlendState(Render_RendererHandle renderer,
+																																 Render_StockBlendType stock);
+AL2O3_EXTERN_C Render_DepthStateHandle Render_GetStockDepthState(Render_RendererHandle renderer,
+																																 Render_StockDepthStateType stock);
+AL2O3_EXTERN_C Render_RasteriserStateHandle Render_GetStockRasterState(Render_RendererHandle renderer,
+																																			 Render_StockRasterState stock);
+AL2O3_EXTERN_C Render_SamplerHandle Render_GetStockSampler(Render_RendererHandle renderer,
+																													 Render_StockSamplerType stock);
+AL2O3_EXTERN_C Render_VertexLayoutHandle Render_GetStockVertexLayout(Render_RendererHandle renderer,
+																																		 Render_StockVertexLayouts stock);
--- include/render_basics/framebuffer.h
+++ include/render_basics/framebuffer.h
@@ -3,6 +3,17 @@
 #include "al2o3_platform/platform.h"
 #include "render_basics/api.h"
 
+typedef struct Render_FrameBufferDesc {
+	void *platformHandle;            ///< platform specific for the window/display (HWND etc.)
+	Render_QueueHandle queue;          ///< queue present will be from
+	Render_CmdPoolHandle commandPool; ///< pool to use for new frame cmd
+	uint32_t frameBufferWidth;        ///< size of the framebuffer horizontally in pixels
+	uint32_t frameBufferHeight;        ///< size of the framebuffer vertically in pixels
+	uint32_t frameBufferCount;        ///< how many frames can be buffered
+	TinyImageFormat colourFormat;      ///< swap chain format, UNDEFINED == platform recommended
+	TinyImageFormat depthFormat;      ///< swap chain depth format, UNDEFINED == none
+} Render_FrameBufferDesc;
+
 AL2O3_EXTERN_C Render_CmdHandle Render_FrameBufferNewFrame(Render_FrameBufferHandle handle,
 																												 Render_RenderTargetHandle *outRenderTarget,
 																												 Render_RenderTargetHandle *outDepthTarget);
